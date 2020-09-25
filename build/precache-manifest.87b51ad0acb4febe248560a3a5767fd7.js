05f9027a3f36651dd2ef9f5727e04a4f15d0d0a6
name <email>
2020-09-25 14:34:53 -0300

    base

---
+++ build/precache-manifest.87b51ad0acb4febe248560a3a5767fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12bc9405a92472661ae874a044b00bbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58778355f5658054804b",
+    "url": "./static/css/main.e9740f9a.chunk.css"
+  },
+  {
+    "revision": "fe700017864b0ebeb5bf",
+    "url": "./static/js/2.284a8b21.chunk.js"
+  },
+  {
+    "revision": "7943b57f2640efa6afaf2291688bb4ff",
+    "url": "./static/js/2.284a8b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58778355f5658054804b",
+    "url": "./static/js/main.dd7d3482.chunk.js"
+  },
+  {
+    "revision": "93c6bd53d3a0cf3118e0",
+    "url": "./static/js/runtime-main.31ddbe0f.js"
+  }
+]);
